7e68b9fbd73d96ec0b942eea07bc05728688f630
LonggTran <longgtran2k4@gmail.com>
2026-03-14 10:44:38 +0700

    #6 - feat: add global exception handling and API response

---
+++ src/main/java/com/projecttaskmanager/backend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.projecttaskmanager.backend.exceptions;
+
+import com.projecttaskmanager.backend.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<ApiResponse<?>> handleAppException(AppException ex) {
+
+        ErrorCode errorCode = ex.getErrorCode();
+
+        ApiResponse<?> response = ApiResponse.builder()
+                .success(false)
+                .message(errorCode.getMessage())
+                .errorCode(errorCode.getCode())
+                .timestamp(Instant.now())
+                .build();
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleGeneralException(Exception ex) {
+
+        ApiResponse<?> response = ApiResponse.builder()
+                .success(false)
+                .message("Unexpected error occurred")
+                .errorCode(ErrorCode.INTERNAL_ERROR.getCode())
+                .timestamp(Instant.now())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
