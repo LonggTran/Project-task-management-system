de3a9627b39d32d520162f149c600e5c8816af97
LonggTran <longgtran2k4@gmail.com>
2026-03-17 01:59:04 +0700

    feat: add JWT authentication

---
+++ src/main/java/com/projecttaskmanager/backend/services/JwtService.java
@@ -0,0 +1,15 @@
+package com.projecttaskmanager.backend.services;
+
+import com.projecttaskmanager.backend.models.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+
+    String generateAccessToken(User user);
+
+    String generateRefreshToken(User user);
+
+    String extractEmail(String token);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
